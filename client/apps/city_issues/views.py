c9a485a8563a6fefd47d083c4615c1d85eeeffe4
Ivan <ultramarinus2009@rambler.ru>
2017-11-20 00:08:00 +0200

    added issues description, code refactoring

--- client/apps/city_issues/views.py
+++ client/apps/city_issues/views.py
@@ -4,6 +4,7 @@ Django views
 # -*- coding: utf-8 -*-
 import json
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
 from django.template import loader
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
@@ -17,17 +18,14 @@ class HomePageView(TemplateView):
 
 def map_page_view(request):
     """Map page"""
-
-    data = serializers.serialize("json", Issues.objects.all())
-
-    context = {
-        'data': data, }
-
-    template = loader.get_template('map_page.html')
-
-    return HttpResponse(template.render(context, request))
+    return render(request, 'map_page.html')
 
 
 def get_issue_data(request, issue_id):
     data = serializers.serialize("json", Issues.objects.filter(pk=issue_id))
+    return JsonResponse(data, safe=False)
+
+
+def get_all_issues_data(request):
+    data = serializers.serialize("json", Issues.objects.all())
     return JsonResponse(data, safe=False)
