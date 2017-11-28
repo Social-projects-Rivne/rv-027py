3a2d82fa2485015e716b335e304bd066b989eea9
KostSvitlana <kost.svitlana.ed@ukr.net>
2017-11-28 21:26:32 +0200

    issue page, rename Issue.name to Issue.title, modal window when restored issue

--- backend/static/assets/js/app.js
+++ backend/static/assets/js/app.js
@@ -3,7 +3,21 @@ $( document ).ready(function() {
         var elemId = $(e.relatedTarget).data('elem-id');
         var elemName = $(e.relatedTarget).data('elem-name');
         var funcName = $(e.relatedTarget).data('func-name');
-        $(".modal-title").text("Confirm deletion of " + elemName);
+        if (funcName == 'deleteissue'){
+            buttonText ='Delete';
+            bodyText = "Please click "+buttonText+" button if you want to delete data.";
+        }
+        else {
+            buttonText ='Restore';
+            bodyText = "Please click "+buttonText+" button if you want to restore data.";
+        }
+        $(".modal-title").text("Confirm operation of " + elemName);
+        $(".button-confirm").text(buttonText);
+        $(".modal-body").text(bodyText);
         $("#delete-user").attr('action', '/'+funcName+'/' + elemId);
+    });
+
+    $('.table-row').click(function() {
+        window.document.location = $(this).data('href');
     });
 });
